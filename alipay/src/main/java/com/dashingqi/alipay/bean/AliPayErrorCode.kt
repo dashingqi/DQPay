e36f48dc3d50883ecc35e9b2f5f15032d00fcad7
DashingQi <m13478493231@163.com>
2020-08-06 17:20:14 +0800

    [优化微信支付和支付宝支付的参数检查以及失败的提醒分类]

--- alipay/src/main/java/com/dashingqi/alipay/bean/AliPayErrorCode.kt
+++ alipay/src/main/java/com/dashingqi/alipay/bean/AliPayErrorCode.kt
@@ -11,16 +11,38 @@ object AliPayErrorCode {
      * 订单参数错误
      */
     const val ALI_PAY_ORDER_PARAMS_ERROR = -100
-
-    private const val ALI_PAY_ORDER_PARAMS_ERROR_STR = "支付信息参数错误"
+    private const val ALI_PAY_ORDER_PARAMS_ERROR_MSG = "支付信息参数错误"
 
     const val ALI_PAY_FAILED_ERROR = -101
-    private const val ALI_PAY_FAILED_ERROR_STR = "支付失败"
+    private const val ALI_PAY_FAILED_ERROR_MSG = "支付失败"
+
+    /**
+     * 数据实体为null
+     */
+    const val ALI_PAY_DATA_BEAN_IS_NULL = -50
+    private const val ALI_PAY_DATE_BEAN_IS_NULL_MSG = "微信支付实体数据为空了哟！"
+
+
+    /**
+     * 传入的回调为null
+     */
+    const val ALI_PAY_CALL_BACK_IS_NULL = -200
+    private const val ALI_PAY_CALL_BACK_IS_NULL_MSG = "微信支付回调不能为空哟！"
+
+
+    /**
+     * 没有安装支付宝
+     */
+    const val ALI_IS_NOT_INSTALL = -400
+    private const val ALI_IS_NOT_INSTALL_MSG = "请安装支付宝！"
 
     private val errorMsgMap by lazy {
         var errorMap = mutableMapOf<Int, String>()
-        errorMap[ALI_PAY_ORDER_PARAMS_ERROR] = ALI_PAY_ORDER_PARAMS_ERROR_STR
-        errorMap[ALI_PAY_FAILED_ERROR] = ALI_PAY_FAILED_ERROR_STR
+        errorMap[ALI_PAY_ORDER_PARAMS_ERROR] = ALI_PAY_ORDER_PARAMS_ERROR_MSG
+        errorMap[ALI_PAY_FAILED_ERROR] = ALI_PAY_FAILED_ERROR_MSG
+        errorMap[ALI_PAY_DATA_BEAN_IS_NULL] = ALI_PAY_DATE_BEAN_IS_NULL_MSG
+        errorMap[ALI_PAY_CALL_BACK_IS_NULL] = ALI_PAY_CALL_BACK_IS_NULL_MSG
+        errorMap[ALI_IS_NOT_INSTALL] = ALI_IS_NOT_INSTALL_MSG
         errorMap
     }
 
--- wxpay/src/main/java/com/dashingqi/wxpay/impl/WXPay.kt
+++ wxpay/src/main/java/com/dashingqi/wxpay/impl/WXPay.kt
@@ -2,9 +2,13 @@ package com.dashingqi.wxpay.impl
 
 import android.app.Activity
 import android.content.Context
+import android.widget.Toast
 import com.dashingqi.dqpay.callback.IPayCallback
 import com.dashingqi.dqpay.strategy.IPayStrategy
 import com.dashingqi.wxpay.bean.WXPayErrorCode
+import com.dashingqi.wxpay.bean.WXPayErrorCode.WX_IS_NOT_INSTALL
+import com.dashingqi.wxpay.bean.WXPayErrorCode.WX_PAY_CALL_BACK_IS_NULL
+import com.dashingqi.wxpay.bean.WXPayErrorCode.WX_PAY_INIT_FAIL
 import com.dashingqi.wxpay.bean.WXPayInfoBean
 import com.tencent.mm.opensdk.modelpay.PayReq
 import com.tencent.mm.opensdk.openapi.IWXAPI
@@ -52,24 +56,68 @@ object WXPay : IPayStrategy<WXPayInfoBean> {
     ) {
         mPayInfo = payInfo
         mPayCallBack = callBack
-        if (callBack == null) {
-            return
+
+        if (checkPrePay()) {
+            //发起微信支付
+            val request = PayReq()
+            request.appId = payInfo.appId
+            request.partnerId = payInfo.partnerId
+            request.sign = payInfo.sign
+            request.timeStamp = payInfo.timeStamp
+            request.prepayId = payInfo.prepayId
+            request.nonceStr = payInfo.nonceStr
+            request.packageValue = payInfo.packageValue
+            mWxApi.sendReq(request)
+        }
+    }
+
+    /**
+     * 支付前的检查
+     */
+    private fun checkPrePay(): Boolean {
+
+        //回调
+        if (mPayCallBack == null) {
+            Toast.makeText(
+                mContext,
+                WXPayErrorCode.getErrorStr(WX_PAY_CALL_BACK_IS_NULL),
+                Toast.LENGTH_LONG
+            ).show()
+            return false
         }
 
-        if (payInfo.appId == "" || payInfo.partnerId == "" || payInfo.sign == "" || payInfo.prepayId == "") {
-            mPayCallBack.onFail(WXPayErrorCode.WX_PAY_ORDER_PARAMS_ERROR,WXPayErrorCode.getErrorStr(WXPayErrorCode.WX_PAY_ORDER_PARAMS_ERROR)!!)
-            return
+        //初始化
+        if (mContext == null || mAppId == null) {
+            mPayCallBack.onFail(
+                WX_PAY_INIT_FAIL,
+                WXPayErrorCode.getErrorStr(WX_PAY_INIT_FAIL)
+            )
+            return false
+        }
+        //判断一下手机上是否安装微信
+
+        if (!mWxApi.isWXAppInstalled) {
+            mPayCallBack.onFail(WX_IS_NOT_INSTALL, WXPayErrorCode.getErrorStr(WX_IS_NOT_INSTALL))
+            return false
         }
 
-        //发起微信支付
-        val request = PayReq()
-        request.appId = payInfo.appId
-        request.partnerId = payInfo.partnerId
-        request.sign = payInfo.sign
-        request.timeStamp = payInfo.timeStamp
-        request.prepayId = payInfo.prepayId
-        request.nonceStr = payInfo.nonceStr
-        request.packageValue = payInfo.packageValue
-        mWxApi.sendReq(request)
+        //支付参数的验证
+        if (mPayInfo != null) {
+            if (mPayInfo.appId == "" || mPayInfo.partnerId == "" || mPayInfo.sign == "" || mPayInfo.prepayId == "") {
+                mPayCallBack.onFail(
+                    WXPayErrorCode.WX_PAY_ORDER_PARAMS_ERROR,
+                    WXPayErrorCode.getErrorStr(WXPayErrorCode.WX_PAY_ORDER_PARAMS_ERROR)!!
+                )
+                return false
+            }
+        } else {
+            mPayCallBack.onFail(
+                WXPayErrorCode.WX_PAY_DATA_BEAN_IS_NULL,
+                WXPayErrorCode.getErrorStr(WXPayErrorCode.WX_PAY_DATA_BEAN_IS_NULL)
+            )
+            return false
+        }
+
+        return true
     }
 }
